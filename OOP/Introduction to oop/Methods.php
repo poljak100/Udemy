8f15d9add682c75afe788849d77e4b2cd3c0ce27
Dino <104674307+poljak100@users.noreply.github.com>
2022-08-01 20:18:38 +0200

    Section1

--- OOP/Introduction to oop/Methods.php
+++ OOP/Introduction to oop/Methods.php
@@ -4,15 +4,20 @@ class basket
 {
     public $itemsTotal;
     public $shippingCost;
+    public $discount;
 
     public function calculateSubTotal()
     {
-        $this->itemsTotal + $this->shippingCost;
+        $subTotal = $this->itemsTotal + $this->shippingCost - $this->discount;
+
+        return $subTotal;
     }
 }
 
 $basket = new basket();
 $basket->itemsTotal = 50;
 $basket->shippingCost = 10;
+$basket->discount = 30;
 
-var_dump($basket);
+
+var_dump($basket->calculateSubTotal());
