0ad1de4c553b64de7090a00edb218c0e7f45674b
Dino <104674307+poljak100@users.noreply.github.com>
2022-08-03 13:10:00 +0200

    Update

---
+++ OOPHP/Principles and Concepts/ClasConstructors/Constructors.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comment</title>
+</head>
+
+<body>
+    <?php
+    require_once 'Comment.php';
+    $comment = new comment('This is some comment text !', 10);
+    ?>
+
+    <p><?php echo $comment->text ?> by user:<?php echo $comment->userId ?></p>
+
+</body>
+
+</html>
