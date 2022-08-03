a79d0268c869b750f753fc40e6a86d10f2509f73
Dino <104674307+poljak100@users.noreply.github.com>
2022-08-03 13:22:35 +0200

    Section2

--- OOPHP/SectionTwo/Principles and Concepts/ClasConstructors/Comment.php
+++ OOPHP/SectionTwo/Principles and Concepts/ClasConstructors/Comment.php
@@ -10,3 +10,4 @@ class Comment
         $this->userId = $userId;
     }
 }
+//
--- OOPHP/SectionTwo/Principles and Concepts/ClasConstructors/Constructors.php
+++ OOPHP/SectionTwo/Principles and Concepts/ClasConstructors/Constructors.php
@@ -14,6 +14,7 @@
     $comment = new comment('This is some comment text !', 10);
     ?>
 
+
     <p><?php echo $comment->text ?> by user:<?php echo $comment->userId ?></p>
 
 </body>
