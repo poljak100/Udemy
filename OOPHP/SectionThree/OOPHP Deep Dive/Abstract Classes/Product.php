4c00b167fc956fa5f8a0869bfa4e8da1070f4488
Dino <104674307+poljak100@users.noreply.github.com>
2022-08-13 20:53:49 +0200

    Section3

--- OOPHP/SectionThree/OOPHP Deep Dive/Abstract Classes/Product.php
+++ OOPHP/SectionThree/OOPHP Deep Dive/Abstract Classes/Product.php
@@ -12,5 +12,31 @@ class Product extends DataModel
     {
         return $this->name;
     }
+
+    /**
+     * @param string $ name
+     */
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return float
+     */
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param float $price
+     */
+
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
 }
-//...
