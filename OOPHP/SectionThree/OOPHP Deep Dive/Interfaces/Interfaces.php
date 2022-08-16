825101480bc6c7585bc7ca904bfeeba45879c4e6
Dino <104674307+poljak100@users.noreply.github.com>
2022-08-16 18:56:10 +0200

    Section3

---
+++ OOPHP/SectionThree/OOPHP Deep Dive/Interfaces/Interfaces.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Interfaces</title>
+</head>
+
+<body>
+    <?php
+
+    require 'DarkSkyApiClient.php';
+    require 'OpenWeatherMapClient.php';
+
+    $weatherApiClient = new OpenWeatherMapClient();
+    $forecast = $weatherApiClient->getForecast('Osijek');
+
+    ?>
+    <h1>Weather App</h1>
+    <p><?php echo $forecast; ?></p>
+</body>
+
+</html>
