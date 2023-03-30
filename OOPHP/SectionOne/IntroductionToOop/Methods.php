830c74367940808948868c201f7c8928c04a372f
Dino Poljak <104674307+poljak100@users.noreply.github.com>
2023-03-30 19:26:36 +0200

    Update Methods.php

--- OOPHP/SectionOne/IntroductionToOop/Methods.php
+++ OOPHP/SectionOne/IntroductionToOop/Methods.php
@@ -17,8 +17,32 @@ class basket
 $basket = new basket();
 $basket->itemsTotal = 100;
 $basket->shippingCost = 30;
-$basket->discount = 30;
+$basket->discount = 50;
 
 $subTotal = $basket->calculateSubTotal();
 
 var_dump($subTotal);
+
+echo '<pre/>';
+
+class Car
+{
+    public $engine;
+    public $color;
+    public $year;
+
+
+    public function NewCar()
+    {
+        $car = $this->engine . '<pre/>' . $this->color . '<pre/>' . $this->year;
+        return $car;
+    }
+}
+
+$New = new Car();
+$New->engine = 'V12';
+$New->color = 'Blue';
+$New->year = '1800';
+
+$car = $New->NewCar();
+print_r($car);
