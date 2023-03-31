d95c24fb5132e053e1b380ab8fccc57e1cebd875
Dino Poljak <104674307+poljak100@users.noreply.github.com>
2023-03-31 22:09:11 +0200

    Update Constructors.php

--- OOPHP/SectionTwo/Principles and Concepts/ClasConstructors/Constructors.php
+++ OOPHP/SectionTwo/Principles and Concepts/ClasConstructors/Constructors.php
@@ -5,17 +5,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comment</title>
+    <title>Document</title>
 </head>
 
 <body>
+
     <?php
     require_once 'Comment.php';
-    $comment = new comment('This is some comment text !', 10);
+
+    $comment = new Comment('Moje je ime Dino, ', 29, 'I dolazim iz Hrvatske')
+
     ?>
 
-
-    <p><?php echo $comment->text ?> by user:<?php echo $comment->userId ?></p>
+    <p><?php echo $comment->name ?> Imam <?php echo $comment->age ?> godina , <?php echo $comment->country ?></p>
 
 </body>
 
