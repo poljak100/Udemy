1ddf57fd049d97c103163bbb8daa0e04ba7c291c
Dino Poljak <104674307+poljak100@users.noreply.github.com>
2023-04-04 23:19:15 +0200

    -

--- OOPHP/SectionThree/OOPHP Deep Dive/MagicMethods/Connection.php
+++ OOPHP/SectionThree/OOPHP Deep Dive/MagicMethods/Connection.php
@@ -1,12 +1,9 @@
 <?php
 
+
 class Connection
 {
-    private static int $count = 0;
-    private string $connectionId;
-    private int $conferenceId = 1234;
-
-
+    private static $count = 0;
 
     public function __construct()
     {
@@ -16,32 +13,5 @@ class Connection
     public function getCount()
     {
         return self::$count;
-    }
-
-    public function __destruct()
-    {
-        self::$count--;
-    }
-
-
-    public function __get($name)
-    {
-        return $this->$name;
-    }
-
-
-    public function setConnectionId($ipAddress)
-    {
-        if (filter_var($ipAddress, FILTER_VALIDATE_IP)) {
-
-            $this->connectionId = $ipAddress . '_' . self::$count;
-            return;
-        }
-        die('not a valid IP address');
-    }
-
-    public function getConnectionId()
-    {
-        return $this->connectionId;
     }
 }
--- OOPHP/SectionThree/OOPHP Deep Dive/MagicMethods/Magic-Methods.php
+++ OOPHP/SectionThree/OOPHP Deep Dive/MagicMethods/Magic-Methods.php
@@ -5,22 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Magic Methods</title>
+    <title>Document</title>
 </head>
 
 <body>
     <?php
     require_once 'Connection.php';
     $connection1 = new Connection();
-    $connection1->setConnectionId('127.0.0.1');
-    //$connection2 = new Connection();
-    //unset($connection2);
 
     ?>
-    <p><?php echo 'The number of connection is' . $connection1->getCount(); ?></p>
-    <p><?php echo 'The conference ID is' . $connection1->$conferenceId; ?></p>
-    <p><?php echo 'The connection ID is' . $connection1->connectionId; ?></p>
 
+    <p><?php echo 'Im connection to: ' . $connection1->getCount() ?></p>
 </body>
 
 </html>
