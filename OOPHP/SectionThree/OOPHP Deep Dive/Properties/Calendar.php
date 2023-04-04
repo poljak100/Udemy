22c96617903062f45f33c5d614dc1cf9628308e0
Dino Poljak <104674307+poljak100@users.noreply.github.com>
2023-04-04 23:05:10 +0200

    Properties

--- OOPHP/SectionThree/OOPHP Deep Dive/Properties/Calendar.php
+++ OOPHP/SectionThree/OOPHP Deep Dive/Properties/Calendar.php
@@ -1,12 +1,26 @@
 <?php
 
+define('YEAR', 2023);
+
 class Calendar
 {
-    public $name;
+    public  $name;
+    public $weeks = 365 * 7;
+    public $year = YEAR;
+
+    public static $days = '28';
+
+
+    public const MONTHS_IN_YEAR = 12;
+
+    public function getMonths()
+    {
+        return self::MONTHS_IN_YEAR;
+    }
 }
 
 $calendar = new Calendar();
-
-$calendar->name = 'Year Planner';
-
-print $calendar->name . PHP_EOL;
+print $calendar->name = 'Dino' . '<br>';
+print Calendar::$days . '<br>';
+print Calendar::MONTHS_IN_YEAR . '<br>';
+print_r($calendar->year . PHP_EOL);
