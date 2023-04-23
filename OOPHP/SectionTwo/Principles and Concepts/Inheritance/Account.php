168fcc1ada0a03c617f3132019cd4409f0f5643d
Dino Poljak <104674307+poljak100@users.noreply.github.com>
2023-04-23 18:52:21 +0200

    2

--- OOPHP/SectionTwo/Principles and Concepts/Inheritance/Account.php
+++ OOPHP/SectionTwo/Principles and Concepts/Inheritance/Account.php
@@ -2,16 +2,16 @@
 
 class Account
 {
-    public $balance = 2;
+    public $balance;
 
     public function withdraw($key)
     {
         echo 'Im withdrawing: ' . $key . '<br>';
     }
 
-    public function deposite($amount)
+    public function deposite($qb)
     {
-        echo 'Im depositing: ' . $amount . '<br>';
+        echo 'Im depositing: ' . $qb . '<br>';
     }
 
     public function GetBalance()
