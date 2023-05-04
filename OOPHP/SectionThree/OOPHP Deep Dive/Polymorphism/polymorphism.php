ce66c82db6e25adff4b8c004b4f5e6179d04c807
Dino Poljak <104674307+poljak100@users.noreply.github.com>
2023-05-04 18:33:18 +0200

    poly

--- OOPHP/SectionThree/OOPHP Deep Dive/Polymorphism/polymorphism.php
+++ OOPHP/SectionThree/OOPHP Deep Dive/Polymorphism/polymorphism.php
@@ -1,14 +1,15 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Polymorphism</title>
 </head>
+
 <body>
-<?php
+    <?php
 
     require_once 'StockManager.php';
     require_once 'JsonFileReader.php';
@@ -19,6 +20,7 @@
     $stockManager->updateStockFromFile('inventory.csv', $csvFileReader);
     $stockManager->updateStockFromFile('inventory.json', $jsonFileReader);
 
-?>
+    ?>
 </body>
+
 </html>
